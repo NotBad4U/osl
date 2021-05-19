195742e6f959e66976934433b902e3bb125ef7b0
Alessio C <alessio.coltellacci@ntu.edu.sg>
2021-05-19 18:05:04 +0800

    Add function definition

--- cfrontend/test.c
+++ cfrontend/test.c
@@ -1,3 +1,23 @@
+int max (int num1, int num2) {
+    if (num1) {
+        result
+    }
+    else {
+        result
+    }
+}
+
+if ( a = 4 ) {
+    if ( a = 4 ) { 
+       a
+    } else {
+        while (1) {
+            return;
+        }
+    }
+} else { 
+    b
+}
 if ( a = 4 ) { 
     if ( a = 4 ) { 
         q
