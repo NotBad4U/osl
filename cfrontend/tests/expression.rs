cb2978d45d2aa672c331db4c3a5f0ebf0d6923a5
Alessio C <alessio.coltellacci@ntu.edu.sg>
2021-08-12 17:40:42 +0800

    Add integration tests to cover assign statement

--- cfrontend/tests/expression.rs
+++ cfrontend/tests/expression.rs
@@ -49,3 +49,46 @@ call main();
 
     assert_equal_program(c_program, expected_osl_program);
 }
+
+
+#[test]
+fn it_should_transpile_assign_constant() {
+    let c_program = r###"
+    void main() {
+        int a;
+        a = 5;
+    }
+    "###;
+
+    let expected_osl_program = r###"
+fn main() -> #own(mut) {
+    decl a;
+    transfer newResource(copy, mut) a;
+}
+call main();
+"###;
+
+    assert_equal_program(c_program, expected_osl_program);
+}
+
+
+#[test]
+fn it_should_transpile_semantic_move() {
+    let c_program = r###"
+    void main() {
+        int a, b;
+        a = b;
+    }
+    "###;
+
+    let expected_osl_program = r###"
+fn main() -> #own(mut) {
+    decl a;
+    decl b;
+    transfer a b;
+}
+call main();
+"###;
+
+    assert_equal_program(c_program, expected_osl_program);
+}
