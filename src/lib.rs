67ec5f9d99547970525b4ebde58af4e682b424c2
NotBad4U <lightplay8@gmail.com>
2021-08-31 19:03:26 +0800

    Split osl binary in lib/bin for futur automatic integration tests

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use cfrontend::configuration::Configuration;
+use lang_c::driver::Config;
+use regex::Regex;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
+
+//TODO: Check in $PATH also
+pub fn are_k_bins_installed() -> bool {
+    return Path::new("k/bin/kompile").exists()
+        && Path::new("k/bin/krun").exists()
+        && Path::new("k/bin/k-configure-opam").exists();
+}
+
+pub fn recompile_model() -> Result<(), Box<dyn Error>> {
+    Command::new("k/bin/kompiled")
+        .args(&["-d", "model"])
+        .output()?;
+    Command::new("k/bin/k-configure-opam").output()?;
+
+    Ok(())
+}
+
+pub fn is_osl_model_compiled() -> bool {
+    Path::new("model/osl-kompiled").exists()
+}
+
+pub fn transpile_c_code(input: &PathBuf) -> Result<cfrontend::ast::Stmts, Box<dyn Error>> {
+    let ast = lang_c::driver::parse(&Config::default(), input.to_string_lossy().into_owned())?;
+    let stmts = cfrontend::transpile_c_program(ast, Configuration::new(true));
+
+    Ok(stmts)
+}
+
+// Run the krun script and get the output
+pub fn krun(file: &PathBuf) -> Result<String, Box<dyn Error>> {
+    let output = Command::new("k/bin/krun")
+        .args(&["-d", "model"])
+        .arg(format!("{}", file.to_string_lossy()))
+        .output()?;
+
+    if output.status.success() == false {
+        // stderr may not crash the program
+        convert_output(output.stderr.as_slice())?;
+    }
+
+    Ok(String::from_utf8(output.stdout.clone())?)
+}
+
+pub fn setup_opam() -> Result<(), Box<dyn Error>> {
+    Command::new("eval").arg("$(opam env)").spawn();
+    Ok(())
+}
+
+pub fn convert_output(output: &[u8]) -> Result<&str, Box<dyn Error>> {
+    std::str::from_utf8(output).map_err(|e| e.into())
+}
+
+/// Check if the PGM in output is equal to <k>.<\k>
+pub fn is_valid(k_output: &str) -> bool {
+    Regex::new(r"<k>\n\s*\.\n\s*</k>")
+        .unwrap()
+        .is_match(k_output)
+}
+
+
+pub fn save_in_file(path_tmp_file: &PathBuf, stmts: cfrontend::ast::Stmts) -> Result<(), Box<dyn Error>> {
+    let mut file = File::create(path_tmp_file)?;
+    file.write_all(
+        format!("{}", cfrontend::ast::render::render_program(stmts))
+            .into_bytes()
+            .as_slice(),
+    )?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 #[macro_use]
 extern crate log;
 
-use cfrontend::configuration::Configuration;
 use env_logger::Builder;
-use lang_c::driver::Config;
 use log::LevelFilter;
-use regex::Regex;
 use structopt::StructOpt;
 
 use std::error::Error;
 use std::fmt;
 use std::fs::File;
 use std::io::{self, Write};
-use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
+
+use osl::*;
 
 #[derive(Debug)]
 enum OslError {
@@ -80,11 +77,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         builder.filter(None, LevelFilter::Info).init();
     }
 
-    let osl_file = opt.output.clone().unwrap_or(PathBuf::from(format!(
-        "{}.osl",
-        opt.input.to_string_lossy()
-    )));
-
     info!("[1/5] Checking K installation");
     if are_k_bins_installed() == false {
         return Err(Box::new(OslError::KbinsMissing));
@@ -100,105 +92,35 @@ fn main() -> Result<(), Box<dyn Error>> {
     setup_opam()?;
 
     info!("[4/5] Transpiling {}", opt.input.to_string_lossy());
-    transpile_c_code(&opt, &osl_file.to_string_lossy())?;
+    let stmts = transpile_c_code(&opt.input)?;
 
-    if opt.only_transpile == false {
-        info!("[5/5] Running OSL");
-        let k_output = krun(&osl_file)?;
+    if opt.only_transpile {
+        info!("[5/5] dry-run mode active, krun will not be started");
+        println!("{}", cfrontend::ast::render::render_program(stmts));
+        return Ok(());
+    }
 
-        if is_valid(k_output.as_str()) {
-            info!("Program valid ✓")
-        } else {
-            error!("Program invalid ✘");
-            io::stdout().write_all(k_output.as_bytes())?;
-        }
+    let osl_file_tmp = opt.output.clone().unwrap_or(PathBuf::from(format!(
+        "{}.osl",
+        opt.input.to_string_lossy()
+    )));
+
+    save_in_file(&osl_file_tmp, stmts);
+
+    info!("[5/5] Running OSL");
+    let k_output = krun(&osl_file_tmp)?;
+
+    if is_valid(k_output.as_str()) {
+        info!("Program valid ✓")
     } else {
-        info!("[5/5] dry-run mode active, krun will not be started")
+        error!("Program invalid ✘");
+        io::stdout().write_all(k_output.as_bytes())?;
     }
 
     if opt.keep == false {
-        trace!("Deleting OSL file {}", osl_file.to_string_lossy());
-        std::fs::remove_file(osl_file)?;
+        trace!("Deleting OSL file {}", osl_file_tmp.to_string_lossy());
+        std::fs::remove_file(osl_file_tmp)?;
     }
 
     Ok(())
-}
-
-//TODO: Check in $PATH also
-fn are_k_bins_installed() -> bool {
-    return Path::new("k/bin/kompile").exists()
-        && Path::new("k/bin/krun").exists()
-        && Path::new("k/bin/k-configure-opam").exists();
-}
-
-fn recompile_model() -> Result<(), Box<dyn Error>> {
-    trace!("k/bin/kompiled -d model");
-    Command::new("k/bin/kompiled")
-        .args(&["-d", "model"])
-        .output()?;
-
-    trace!("k/bin/k-configure-opam");
-    Command::new("k/bin/k-configure-opam").output()?;
-
-    Ok(())
-}
-
-fn is_osl_model_compiled() -> bool {
-    Path::new("model/osl-kompiled").exists()
-}
-
-fn transpile_c_code(opt: &Opt, output_file_name: &str) -> Result<(), Box<dyn Error>> {
-    info!("parsing {}...", opt.input.to_string_lossy());
-    let ast = lang_c::driver::parse(&Config::default(), opt.input.to_string_lossy().into_owned())?;
-
-    info!("transpiling {}...", opt.input.to_string_lossy());
-    let stmts = cfrontend::transpile_c_program(ast, Configuration::new(true));
-
-    if opt.only_transpile && !opt.keep {
-        println!("{}", stmts);
-    } else {
-        trace!("creating temporary file: {}", output_file_name);
-        let mut file = File::create(output_file_name)?;
-        file.write_all(
-            format!("{}", cfrontend::ast::render::render_program(stmts))
-                .into_bytes()
-                .as_slice(),
-        )?;
-    }
-
-    Ok(())
-}
-
-// Run the krun script and get the output
-fn krun(file: &PathBuf) -> Result<String, Box<dyn Error>> {
-    trace!("k/bin/krun -d model {}", file.to_string_lossy());
-    let output = Command::new("k/bin/krun")
-        .args(&["-d", "model"])
-        .arg(format!("{}", file.to_string_lossy()))
-        .output()?;
-
-    if output.status.success() == false {
-        // stderr may not crash the program
-        let msg = convert_output(output.stderr.as_slice())?;
-        error!("{}\n{}", output.status, msg);
-    }
-
-    Ok(String::from_utf8(output.stdout.clone())?)
-}
-
-fn setup_opam() -> Result<(), Box<dyn Error>> {
-    trace!("eval $(opam env)");
-    Command::new("eval").arg("$(opam env)").spawn();
-    Ok(())
-}
-
-fn convert_output(output: &[u8]) -> Result<&str, Box<dyn Error>> {
-    std::str::from_utf8(output).map_err(|e| e.into())
-}
-
-/// Check if the PGM in output is equal to <k>.<\k>
-fn is_valid(k_output: &str) -> bool {
-    Regex::new(r"<k>\n\s*\.\n\s*</k>")
-        .unwrap()
-        .is_match(k_output)
 }
